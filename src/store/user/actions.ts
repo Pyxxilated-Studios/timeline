e1af2d1c058f34b48e23ab863ad7b0c149840ec7
Joshua Hill <josh@pyxxilated.studio>
2020-08-27 20:57:44 +1200

    Update Linting

--- src/store/user/actions.ts
+++ src/store/user/actions.ts
@@ -1,46 +1,46 @@
 import {
-  UserAction,
-  SET_USERNAME,
-  SET_REPOSITORIES,
-  SET_LOGGED_IN,
-  SET_STATE,
-  SET_TOKEN,
-  SET_FETCHED,
-  LOGOUT,
-} from "./types";
+    UserAction,
+    SET_USERNAME,
+    SET_REPOSITORIES,
+    SET_LOGGED_IN,
+    SET_STATE,
+    SET_TOKEN,
+    SET_FETCHED,
+    LOGOUT,
+} from './types';
 
-import { Repository } from "../../types";
+import { Repository } from '../../types';
 
 export const setUsername = (username: string): UserAction => {
-  return {
-    type: SET_USERNAME,
-    username,
-  };
+    return {
+        type: SET_USERNAME,
+        username,
+    };
 };
 
 export const setRepositories = (repositories: Repository[]): UserAction => {
-  return {
-    type: SET_REPOSITORIES,
-    repositories,
-  };
+    return {
+        type: SET_REPOSITORIES,
+        repositories,
+    };
 };
 
 export const setLoggedIn = (loggedIn: boolean): UserAction => {
-  return { type: SET_LOGGED_IN, loggedIn };
+    return { type: SET_LOGGED_IN, loggedIn };
 };
 
 export const setState = (state: string): UserAction => {
-  return { type: SET_STATE, state };
+    return { type: SET_STATE, state };
 };
 
 export const setToken = (token: string): UserAction => {
-  return { type: SET_TOKEN, token };
+    return { type: SET_TOKEN, token };
 };
 
 export const setFetched = (fetched: boolean): UserAction => {
-  return { type: SET_FETCHED, fetched };
+    return { type: SET_FETCHED, fetched };
 };
 
 export const logout = (): UserAction => {
-  return { type: LOGOUT };
+    return { type: LOGOUT };
 };
